878228cf837cbc888236f2d7303810a8188345fc
Marcos Perez Esteban <149711979+Feelis3@users.noreply.github.com>
2025-06-20 19:19:56 +0200

    Jugador animacion terminada

--- scripts/jugador.gd
+++ scripts/jugador.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 @export var speed := 200
 @onready var anim = $AnimacionJugador
-@onready var sprite = $Sprite  # Asegúrate de cambiar el nombre si es distinto
+@onready var sprite_idle = $SpriteIdle
+@onready var sprite_walk = $SpriteWalk
 
 var last_direction = "down"
 
@@ -15,24 +16,31 @@ func _physics_process(delta):
 	move_and_slide()
 
 	if direccion == Vector2.ZERO:
+		sprite_idle.visible = true
+		sprite_walk.visible = false
 		match last_direction:
+			
 			"up": anim.play("idle_back")
 			"down": anim.play("idle_front")
 			"left", "right": anim.play("idle_right")
 	else:
+		sprite_idle.visible = false
+		sprite_walk.visible = true
 		if abs(direccion.x) > abs(direccion.y):
 			if direccion.x > 0:
-				sprite.flip_h = false
+				sprite_walk.flip_h = false
+				sprite_idle.flip_h = false
 				last_direction = "right"
 			else:
-				sprite.flip_h = true
+				sprite_walk.flip_h = true
+				sprite_idle.flip_h = true
 
 				last_direction = "left"
-			anim.play("idle_right")
+			anim.play("walk_right")
 		else:
 			if direccion.y < 0:
-				anim.play("idle_back")
+				anim.play("walk_up")
 				last_direction = "up"
 			else:
-				anim.play("idle_front")
+				anim.play("walk_front")
 				last_direction = "down"
