16b65409eb24b70a29943121f2c69b6e73aa2c1a
Marcos Perez Esteban <149711979+Feelis3@users.noreply.github.com>
2025-06-20 18:37:47 +0200

    Juego

--- scripts/jugador.gd
+++ scripts/jugador.gd
@@ -3,13 +3,14 @@ extends CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Animaciones.play("Animacion")
+	$Animaciones.play("animacion")
 
 
 func _physics_process(delta):
 	var direccion = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	velocity = direccion*200
 	move_and_slide()
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	
+	
 func _process(delta: float) -> void:
 	pass
