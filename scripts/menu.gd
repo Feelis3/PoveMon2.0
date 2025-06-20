4140379e6bacf960cc8573874fd82eb3035f5e39
LaCabra <149711978+Poveda83@users.noreply.github.com>
2025-06-20 19:30:16 +0200

    cambios con opciones

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,13 +1,11 @@
 extends Node2D
 
-
-func _on_jugar_pressed() -> void:
-	pass # Replace with function body.
-
-
 func _on_opciones_pressed() -> void:
-	pass # Replace with function body.
-
+	get_tree().change_scene_to_file("res://scenes/Opciones.tscn")
 
 func _on_salir_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Juego.tscn")
