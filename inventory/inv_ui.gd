916d125ed031deab1538d49adbdf9298cae9297c
Marcos Perez Esteban <149711979+Feelis3@users.noreply.github.com>
2025-06-20 23:26:19 +0200

    Cambio inventario

--- inventory/inv_ui.gd
+++ inventory/inv_ui.gd
@@ -1,14 +1,42 @@
 extends Control
-
 @onready var inv: Inventory = preload("res://inventory/playerinv.tres")
 @onready var slots: Array = $NinePatchRect/GridContainer.get_children()
-
+@onready var objetoLabel: Label = $NinePatchRect2/Label
 var is_open = false
 
 func _ready():
 	update_slots()
+	connect_slot_signals()
 	close()
+
+func connect_slot_signals():
+	for i in range(slots.size()):
+		if slots[i].has_signal("mouse_entered"):
+			slots[i].mouse_entered.connect(_on_slot_mouse_entered.bind(i))
+			slots[i].mouse_exited.connect(_on_slot_mouse_exited.bind(i))
+
+func _on_slot_mouse_entered(slot_index: int):
+	if slot_index < inv.items.size() and inv.items[slot_index] != null:
 	
+		objetoLabel.text = inv.items[slot_index].name
+
+func _on_slot_mouse_exited(slot_index: int):
+			objetoLabel.text = ""
+
+	
+
+	
+
+func _on_mouse_entered():
+	print("Ratón encima del slot")
+	# Aquí puedes cambiar el color, mostrar tooltip, etc.
+	modulate = Color.YELLOW
+
+func _on_mouse_exited():
+	print("Ratón salió del slot")
+	# Restaurar apariencia normal
+	modulate = Color.WHITE
+
 func update_slots():
 	for i in range(min(inv.items.size(),slots.size())):
 		slots[i].update(inv.items[i])
--- inventory/inv_ui_slot.gd
+++ inventory/inv_ui_slot.gd
@@ -1,6 +1,10 @@
 extends Panel
 
 @onready var item_visual: Sprite2D = $CenterContainer/Panel/item_display
+@onready var item_cant: Label = $CenterContainer/Panel/item_cant
+func _ready():
+	item_visual.scale = Vector2(0.015, 0.015)
+
 
 func update(item: InvItem):
 	if !item:
@@ -8,3 +12,4 @@ func update(item: InvItem):
 	else:
 		item_visual.visible = true
 		item_visual.texture = item.texture
+		item_cant.text = "x" + str(item.cantidad)
